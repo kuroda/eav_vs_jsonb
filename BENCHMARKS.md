aa752e3f622c95257a9aa572c71bd8cd66fa7869
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-03-05 18:10:57 +0900

    Do benchmarks on record creation

---
+++ BENCHMARKS.md
@@ -0,0 +1,15 @@
+# BENCHMARKS
+
+## Inserts
+
+### N = 1,000
+
+Simple:  3.211s
+JSONB:   3.984s
+EAV:    12.471s
+
+### N = 10,000
+
+Simple:  25.162s
+JSONB:   25.701s
+EAV:    127.223s
---
+++ benchmark.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu
+
+export MIX_ENV=test
+export RECORDS=1000
+export TIMEFORMAT=%R
+
+mix ecto.migrate.reset
+
+time mix run priv/repo/seeds/simple.exs
+time mix run priv/repo/seeds/jsonb.exs
+time mix run priv/repo/seeds/eav.exs
