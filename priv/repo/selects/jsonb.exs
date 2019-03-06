ca47fd752cdb159518010acebce52fa4b061ad56
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-03-06 11:35:57 +0900

    Improve selects (jsonb)

--- priv/repo/selects/jsonb.exs
+++ priv/repo/selects/jsonb.exs
@@ -1,15 +1,21 @@
+import Ecto.Query
 alias EavVsJsonb.Repo
+alias EavVsJsonb.JSONB.Record
 
 {records, _} = Integer.parse(System.get_env("RECORDS") || "100")
 
 for i <- 1..records do
-  r = Ecto.Adapters.SQL.query!(Repo, """
-    SELECT data FROM jsonb_records WHERE data @> '{"j":#{i}}'
-  """)
+  map = %{"j" => i}
 
-  data = r.rows |> Enum.at(0) |> Enum.at(0)
+  r =
+    Record
+    |> where(fragment("data @> ?", ^map))
+    |> Repo.one()
+
+  data = r.data
 
   if data["j"] != i do
     IO.puts "Wrong!"
+    IO.inspect data
   end
 end
