a48c34bc36309d8da3d8501cd85c67c9f9b6c3c0
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-03-06 17:04:08 +0900

    Modify seeding script for jsonb_records

--- priv/repo/seeds/jsonb.exs
+++ priv/repo/seeds/jsonb.exs
@@ -1,5 +1,6 @@
 alias EavVsJsonb.Repo
 alias EavVsJsonb.JSONB.Record
+alias EavVsJsonb.JSONB.StringValue
 
 :rand.seed(:exrop, {1, 1, 1})
 
@@ -8,12 +9,17 @@ alias EavVsJsonb.JSONB.Record
 for i <- 1..records do
   r = rem(i, 4)
 
-  Repo.insert!(%Record{
+  record = Repo.insert!(%Record{
     data: %{
       a: :crypto.hash(:md5, "a-#{i}") |> Base.encode16(case: :lower),
       b: ~w(alpha bravo charlie delta) |> Enum.at(r),
       i: :rand.uniform(1000),
       j: i
     }
+  })
+
+  Repo.insert!(%StringValue{
+    record_id: record.id,
+    value: record.data["b"]
   })
 end
